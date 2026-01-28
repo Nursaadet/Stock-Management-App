2adc691a2e17ee0810a17a61216ecbdcba716546
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-28 16:09:19 +0300

    feat: configure redux-persist with auth reducer

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -2,7 +2,16 @@ import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "../features/authSlice"
 import stockReducer from "../features/stockSlice"
 
-import { persistStore, persistReducer } from "redux-persist"
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist"
 import storage from "redux-persist/lib/storage" //? defaults to localStorage
 // import storage from "redux-persist/lib/storage/session" //? session storage
 
@@ -18,6 +27,12 @@ const store = configureStore({
     auth: persistedReducer,
     stock: stockReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
   devTools: process.env.NODE_ENV !== "production",
 })
 
