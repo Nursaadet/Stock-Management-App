eb3d040df7d698f3aea87646dc42f8567b7752f3
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-28 14:16:23 +0300

    feat: add stock redux slice

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,8 +1,19 @@
 import { useEffect } from "react"
+// import useAxios from "../services/useAxios"
 import useStockRequest from "../services/useStockRequest"
+
+// export const getFirms = async () => {
+//   try {
+//     const { data } = axiosToken("/firms")
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
 const Firms = () => {
   const { getFirms } = useStockRequest()
+  // const { axiosToken } = useAxios()
 
   useEffect(() => {
     getFirms()
