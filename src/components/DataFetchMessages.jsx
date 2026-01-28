2508a416d1e754e3178ad73b63a6ad8751eb80e8
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-29 00:02:15 +0300

    feat: add error handling for ProductTable with ErrorMessage component

---
+++ src/components/DataFetchMessages.jsx
@@ -0,0 +1,11 @@
+import Alert from "@mui/material/Alert"
+
+export const ErrorMessage = () => {
+  return <Alert severity="error">Veriler çekilemedi</Alert>
+}
+
+const DataFetchMessages = () => {
+  return <div></div>
+}
+
+export default DataFetchMessages
