c0aadf2aa2ef4d9542b766e22decb0d807d096ce
Nursaadet <nursaadetozdemir@gmail.com>
2026-01-29 15:05:36 +0300

    style: enhance KPI cards UI with MUI Avatar and icons

--- src/components/KPICards.jsx
+++ src/components/KPICards.jsx
@@ -1,7 +1,11 @@
 import Paper from "@mui/material/Paper"
+import Avatar from "@mui/material/Avatar"
 import Stack from "@mui/material/Stack"
+import Box from "@mui/material/Box"
+import { deepOrange, deepPurple } from "@mui/material/colors"
 import EuroIcon from "@mui/icons-material/Euro"
 import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket"
+import { Typography } from "@mui/material"
 const KPICards = () => {
   const kpiData = [
     {
@@ -9,8 +13,8 @@ const KPICards = () => {
       title: "Sales",
       icon: <EuroIcon />,
       amount: "€12000",
-      color: "pink",
-      bgColor: "hotpink",
+      color: deepPurple[700],
+      bgColor: deepPurple[100],
     },
     {
       id: 2,
@@ -24,8 +28,22 @@ const KPICards = () => {
   return (
     <Stack justifyContent={"center"} alignItems={"center"} flexWrap={"wrap"}>
       {kpiData.map((data) => (
-        <Paper key={data.id} elevation={3}>
-          merhaba
+        <Paper key={data.id} elevation={3} sx={{ display: "flex" }}>
+          <Avatar
+            sx={{
+              bgcolor: data.bgColor,
+              color: data.color,
+              width: 60,
+              height: 60,
+            }}
+          >
+            {data.icon}
+          </Avatar>
+
+          <Box>
+            <Typography>{data.title}</Typography>
+            <Typography>{data.amount}</Typography>
+          </Box>
         </Paper>
       ))}
     </Stack>
